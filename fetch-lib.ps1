bcc9de589a0e55aa8a50e4403e217fd88f459dac
Martin Büttner <m.buettner.d@gmail.com>
2014-01-25 13:15:27 +0000

    Add project settings and git subtree scripts

---
+++ fetch-lib.ps1
@@ -0,0 +1,6 @@
+# This script fetches the most recent state of the library repo's src-only
+# branch and merges into the /lib directory of the current branch.
+
+
+git fetch ffphplib src-only
+git subtree pull --prefix=lib ffphplib src-only --squash
