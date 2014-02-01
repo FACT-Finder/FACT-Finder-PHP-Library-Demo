e6babdc839f8b997829e9cf5da68545546285133
Martin Büttner <m.buettner.d@gmail.com>
2014-02-01 16:05:05 +0000

    Add tracking

--- demo/tracking.php
+++ demo/tracking.php
@@ -76,29 +76,12 @@ $dic['requestFactory'] = $dic->share(function($c) {
     );
 });
 
-$suggestAdapter = FF::getInstance(
-    'Adapter\Suggest',
+$trackingAdapter = FF::getInstance(
+    'Adapter\ScicTracking',
     $dic['loggerClass'],
     $dic['configuration'],
     $dic['requestFactory']->getRequest(),
     $dic['clientUrlBuilder']
 );
 
-try {
-	echo $suggestAdapter->getRawSuggestions();
-} catch (\Exception $e) {
-	if (!headers_sent()) {
-		// close connection to browser if that is possible
-		header("Content-Length: 0");
-		header("Connection: close");
-		flush();
-
-		/* if you want, you can log errors here. this will not cause the user to wait for the request * /
-		// log error
-		$logfile = "suggest.error.log";
-		$f = fopen($logfile, 'a');
-		fwrite($f, date(DATE_RFC822).': error ['.$e->getMessage().'] for search request ['.$dataProvider->getAuthenticationUrl()."]\n");
-		fclose($f);
-		// */
-	}
-}
+echo $trackingAdapter->doTrackingFromRequest();
