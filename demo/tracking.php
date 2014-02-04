cf83197c1ecfef589ff21aff50168261fea1b006
Martin Büttner <m.buettner.d@gmail.com>
2014-02-04 13:45:53 +0000

    Factor out initialization code
    Pulled the initialization code common to all PHP scripts into a separate
    file to avoid duplication.

--- demo/tracking.php
+++ demo/tracking.php
@@ -13,68 +13,7 @@ require_once LIB_DIR.DS.'FACTFinder'.DS.'Loader.php';
 
 use FACTFinder\Loader as FF;
 
-// Set up dependency-injection container (Pimple)
-// See https://github.com/fabpot/Pimple for more details
-
-$dic = FF::getInstance('Util\Pimple');
-
-$logClass = FF::getClassName('Util\Log4PhpLogger');
-$logClass::configure(USERDATA_DIR.DS.'log4php.xml');
-
-$dic['loggerClass'] = $logClass;
-
-$dic['configuration'] = $dic->share(function($c) {
-    return FF::getInstance(
-        'Core\XmlConfiguration',
-        USERDATA_DIR.DS.'config.xml',
-        'production'
-    );
-});
-
-$dic['encodingConverter'] = $dic->share(function($c) {
-    if (extension_loaded('iconv'))
-        $type = 'Core\IConvEncodingConverter';
-    else if (function_exists('utf8_encode')
-             && function_exists('utf8_decode'))
-        $type = 'Core\Utf8EncodingConverter';
-    else
-        throw new \Exception('No encoding conversion available.');
-
-    return FF::getInstance(
-        $type,
-        $c['loggerClass'],
-        $c['configuration']
-    );
-});
-
-$dic['requestParser'] = $dic->share(function($c) {
-    return FF::getInstance(
-        'Core\Client\RequestParser',
-        $c['loggerClass'],
-        $c['configuration'],
-        $c['encodingConverter']
-    );
-});
-
-$dic['clientUrlBuilder'] = function($c) {
-    return FF::getInstance(
-        'Core\Client\UrlBuilder',
-        $c['loggerClass'],
-        $c['configuration'],
-        $c['requestParser'],
-        $c['encodingConverter']
-    );
-};
-
-$dic['requestFactory'] = $dic->share(function($c) {
-    return FF::getInstance(
-        'Core\Server\MultiCurlRequestFactory',
-        $c['loggerClass'],
-        $c['configuration'],
-        FF::getInstance('Util\Curl'), // Can be replaced with a stub
-        $c['requestParser']->getRequestParameters()
-    );
-});
+require_once USERDATA_DIR.DS.'initialization.php';
 
 $trackingAdapter = FF::getInstance(
     'Adapter\ScicTracking',
---
+++ demo/userdata/initialization.php
@@ -0,0 +1,73 @@
+<?php
+namespace FACTFinderDemo;
+
+use FACTFinder\Loader as FF;
+
+// Set up dependency-injection container (Pimple)
+// See https://github.com/fabpot/Pimple for more details
+
+$dic = FF::getInstance('Util\Pimple');
+
+// The following logger class will be used everywhere within the library.
+// If required, a second logger class can be created to handle logging outside
+// of the library differently. Alternatively, you can just instantiate this
+// logger with a different scope and configure that scope separately in
+// userdata/log4php.xml.
+
+$logClass = FF::getClassName('Util\Log4PhpLogger');
+$logClass::configure(USERDATA_DIR.DS.'log4php.xml');
+
+$dic['loggerClass'] = $logClass;
+
+$dic['configuration'] = $dic->share(function($c) {
+    return FF::getInstance(
+        'Core\XmlConfiguration',
+        USERDATA_DIR.DS.'config.xml',
+        'production'
+    );
+});
+
+$dic['encodingConverter'] = $dic->share(function($c) {
+    if (extension_loaded('iconv'))
+        $type = 'Core\IConvEncodingConverter';
+    else if (function_exists('utf8_encode')
+             && function_exists('utf8_decode'))
+        $type = 'Core\Utf8EncodingConverter';
+    else
+        throw new \Exception('No encoding conversion available.');
+
+    return FF::getInstance(
+        $type,
+        $c['loggerClass'],
+        $c['configuration']
+    );
+});
+
+$dic['requestParser'] = $dic->share(function($c) {
+    return FF::getInstance(
+        'Core\Client\RequestParser',
+        $c['loggerClass'],
+        $c['configuration'],
+        $c['encodingConverter']
+    );
+});
+
+$dic['clientUrlBuilder'] = function($c) {
+    return FF::getInstance(
+        'Core\Client\UrlBuilder',
+        $c['loggerClass'],
+        $c['configuration'],
+        $c['requestParser'],
+        $c['encodingConverter']
+    );
+};
+
+$dic['requestFactory'] = $dic->share(function($c) {
+    return FF::getInstance(
+        'Core\Server\MultiCurlRequestFactory',
+        $c['loggerClass'],
+        $c['configuration'],
+        FF::getInstance('Util\Curl'), // Can be replaced with a stub
+        $c['requestParser']->getRequestParameters()
+    );
+});
